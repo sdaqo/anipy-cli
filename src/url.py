6cc24ddfb9d9000123d263e27da9ce7749686efa
sdao <63876564+sdaqo@users.noreply.github.com>
2021-12-19 11:17:05 +0100

    Update url.py

--- src/url.py
+++ src/url.py
@@ -1,7 +1,6 @@
 import requests, re, webbrowser, time, subprocess as sp
 from bs4 import BeautifulSoup
 from src.colors import colors
-frä
 
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.44"
