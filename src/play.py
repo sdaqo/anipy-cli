d4d8d0e83f05cc25766f971fabb942b4ee19c7a5
paulsmoech@gmail.com <paulsmoech@gmail.com>
2021-12-29 23:40:32 +0100

    Set media-titel of mpv to show title instead of the gibberish link.

--- src/play.py
+++ src/play.py
@@ -8,11 +8,12 @@ import os
 
 player = "mpv"
 terminate = False
+base_url = "https://gogoanime.wiki/"
 
 def play(embed_url, video_url, link, is_history, start_at="0"):
     global stop, sub_proc
     stop = False
-    player_command = player + " --start=+" + str(start_at) + " --cache=yes " +  '--http-header-fields="Referer: ' + embed_url + '" "' + video_url + '"'
+    player_command = player + f" --force-media-title={link.replace(base_url, '')} " + f" --start=+{str(start_at)}" + " --cache=yes " +  f'--http-header-fields="Referer: {embed_url}" ' + f"'{video_url}'"
     Thread(target=write_history, args=(link, is_history)).start()
     if os.name in ('nt', 'dos'):
         sub_proc = sp.Popen(player_command)
