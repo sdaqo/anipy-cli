0e1ea6ceed77fffc71d40ea3173e6014616952b9
Dabbing Guy <77312570+Dabbing-Guy@users.noreply.github.com>
2022-03-04 00:37:58 -0600

    Add tqdm and pycryptdome as dependencies. Remove pycrypt as a dependency

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     description='Little tool in python to watch anime from the terminal (the better way to watch anime)',
     author='sdaqo',
     license='GPL-3.0',
-    install_requires=['bs4', 'requests', ''],
+    install_requires=['bs4', 'requests', 'tqdm', 'pycryptodome'],
     entry_points="[console_scripts]\nanipy-cli=anipy_cli.cli:main",
 )
