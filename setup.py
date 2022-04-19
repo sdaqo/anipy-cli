647a69320bb8dfbaab357412bf563cb5a92ee44c
sdaqo <paulsmoech@gmail.com>
2022-04-19 16:22:28 +0200

    add kitsu.py to install deps

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
         "tqdm",
         "pycryptodomex",
         "better-ffmpeg-progress",
+        "kitsu.py @ git+https://github.com/dymattic/kitsu.py.git@a1df9865f761fa88f75efd1da62d2d3bfe66d4c5"
     ],
     entry_points="[console_scripts]\nanipy-cli=anipy_cli.run_anipy_cli:main",
 )
