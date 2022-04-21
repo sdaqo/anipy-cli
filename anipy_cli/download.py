0d4b76c18d369650fa95c85d837bd73433f8106e
Dmitrij Pastian <dmitrij.pastian@planit.legal>
2022-04-21 21:41:07 +0200

    fixed multi threaded download, now with audio

--- anipy_cli/download.py
+++ anipy_cli/download.py
@@ -1,12 +1,13 @@
-import json
 import re
 import urllib
 from pathlib import Path
 
+import ffmpeg
 import m3u8
 import requests
 import shutil
 import sys
+
 from tqdm import tqdm
 from requests.adapters import HTTPAdapter, Retry
 from concurrent.futures import ThreadPoolExecutor
@@ -25,11 +26,16 @@ class download:
     """
 
     def __init__(self, entry, ffmpeg=False) -> None:
+        self.is_audio = None
+        self.content_audio_media = None
         self._m3u8_content = None
         self.session = None
         self.entry = entry
         self.ffmpeg = ffmpeg
-        self.headers = {"referer": self.entry.embed_url}
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+            "referer": self.entry.embed_url,
+        }
 
     def download(self):
         self.show_folder = config.download_folder_path / f"{self.entry.show_name}"
@@ -107,41 +113,54 @@ class download:
             error("interrupted deleting partially downloaded file")
             fname.unlink()
 
-    def ffmpeg_merge(self, input_file):
+    def ffmpeg_merge(self, input_file, audio_input_file):
+
         config.user_files_path.mkdir(exist_ok=True)
         config.ffmpeg_log_path.mkdir(exist_ok=True)
         fname = f"{self.entry.show_name}_{self.entry.ep}.mp4"
 
         dl_path = self.show_folder / fname
 
-        ffmpeg_process = FfmpegProcess(
-            [
-                "ffmpeg",
-                "-i",
-                str(input_file),
-                "-vcodec",
-                "copy",
-                "-acodec",
-                "copy",
-                "-scodec",
-                "mov_text",
-                "-c",
-                "copy",
-                str(dl_path),
-            ]
-        )
-
+        merged_video_ts = self.merge_ts_files(input_file)
+        ffmpeg_p = ffmpeg
+        ffmpeg_p
+        input_streams = [ffmpeg.input(merged_video_ts)]
+        if audio_input_file:
+            merged_audio_ts = self.merge_ts_files(audio_input_file,"_audio")
+            input_streams.append(ffmpeg.input(merged_audio_ts))
         try:
             print(f"{colors.CYAN}Merging Parts using ffmpeg...")
-            ffmpeg_process.run(
-                ffmpeg_output_file=str(
-                    config.ffmpeg_log_path / fname.replace("mp4", "log")
-                )
-            )
+            ffmpeg_p.output(
+                *input_streams,
+                str(dl_path),
+                vcodec="copy",
+                acodec="copy",
+                scodec="mov_text",
+                c="copy",
+                loglevel="error",
+
+            ).run()
+
             print(f"{colors.CYAN}Merge finished.")
         except KeyboardInterrupt:
             error("interrupted deleting partially downloaded file")
             fname.unlink()
+
+    def merge_ts_files(self, input_file, suffix=""):
+        filename = f"{self.temp_folder}/{self.entry.show_name}_{self.entry.ep}_merged{suffix}.ts"
+        # Parse playlist for filenames with ending .ts and put them into the list ts_filenames
+        with open(input_file, "r") as playlist:
+            ts_filenames = [
+                line.rstrip()+f"{suffix}" for line in playlist if line.rstrip().endswith(".ts")
+            ]
+        # open one ts_file from the list after another and append them to merged.ts
+        with open(
+                filename, "wb"
+        ) as merged:
+            for ts_file in ts_filenames:
+                with open(ts_file, "rb") as mergefile:
+                    shutil.copyfileobj(mergefile, merged)
+        return filename
 
     def mp4_dl(self, dl_link):
         """
@@ -172,36 +191,42 @@ class download:
 
         print(f"{colors.CYAN}Download finished.")
 
-    def download_ts(self, m3u8_segments):
+    def download_ts(self, m3u8_segments, retry=0):
         self.counter += 1
+        audio_suffix = ""
         uri = urllib.parse.urljoin(m3u8_segments.base_uri, m3u8_segments.uri)
         if not self._is_url(uri):
+            input(f"uri: {uri} is not an uri")
             return
 
-        filename = self._get_filename(uri, self.temp_folder)
+        if self.is_audio:
+            audio_suffix = "audio"
+        filename = self._get_filename(uri, self.temp_folder, audio_suffix)
         headers = self.headers
+        retry_count = 0
+        while not Path(filename).is_file() and retry_count < 20:
+            print(
+                f"{colors.CYAN}Downloading {audio_suffix} Part: {self.counter}/{self.segment_count}",
+                end="",
+            )
+            print("\r", end="")
 
-        print(
-            f"{colors.CYAN}Downloading Part: {self.counter}/{self.segment_count}",
-            end="",
-        )
-        print("\r", end="")
+            try:
+                with self.session.get(
+                        uri, timeout=10, headers=headers, stream=False
+                ) as response:
 
-        try:
-            with self.session.get(
-                uri, timeout=10, headers=headers, stream=False
-            ) as response:
+                    if response.status_code == 416:
+                        return
 
-                if response.status_code == 416:
-                    return
+                    response.raise_for_status()
 
-                response.raise_for_status()
+                    with open(filename, "wb") as fout:
+                        fout.write(response.content)
 
-                with open(filename, "wb") as fout:
-                    fout.write(response.content)
-
-        except Exception as e:
-            exit(e.__str__())
+            except Exception as e:
+                exit(e.__str__())
+            retry_count += 1
 
     def multithread_m3u8_dl(self):
         """
@@ -225,24 +250,36 @@ class download:
             self.entry.stream_url, 10, self.headers
         )
         assert self._m3u8_content.is_variant is False
-        self.segment_count = len(self._m3u8_content.segments)
-        try:
-            with ThreadPoolExecutor(60) as pool:
-                pool.map(self.download_ts, self._m3u8_content.segments)
-            self._download_key()
 
+        try:
+            if not self.content_audio_media.is_variant:
+                self.segment_count = len(self.content_audio_media.segments)
+                self.is_audio = True
+                with ThreadPoolExecutor(12) as pool_audio:
+                    pool_audio.map(self.download_ts, self.content_audio_media.segments)
+            self.is_audio = False
+            self.counter = 0
+            self.segment_count = len(self._m3u8_content.segments)
+            print("\n")
+            with ThreadPoolExecutor(12) as pool_video:
+                pool_video.map(self.download_ts, self._m3u8_content.segments)
         except KeyboardInterrupt:
             shutil.rmtree(self.temp_folder)
             keyboard_inter()
             sys.exit()
-        input_file = self._dump_m3u8()
+
+        input_file = self._dump_m3u8(self._m3u8_content)
+        audio_input_file = None
+        if not self.content_audio_media.is_variant:
+            self.is_audio = True
+            audio_input_file = self._dump_m3u8(self.content_audio_media)
 
         print(f"\n{colors.CYAN}Parts Downloaded")
-        self.ffmpeg_merge(input_file)
+        self.ffmpeg_merge(input_file, audio_input_file)
         print(f"\n{colors.CYAN}Parts Merged")
         shutil.rmtree(self.temp_folder)
 
-    def _download_m3u8(self, uri, timeout, headers):
+    def _download_m3u8(self, uri, timeout, headers, is_audio=False):
         if self._is_url(uri):
             resp = self.session.get(uri, timeout=timeout, headers=self.headers)
             resp.raise_for_status()
@@ -253,14 +290,13 @@ class download:
                 raw_content = fin.read()
                 base_uri = Path(uri)
         content = m3u8.M3U8(raw_content, base_uri=base_uri)
-
-        # sort
-        sorted_content_playlist = sorted(
-            content.playlists, key=lambda x: x.stream_info.bandwidth, reverse=True
-        )
-
         if content.is_variant:
-            for index, playlist in enumerate(sorted_content_playlist):
+            if self.content_audio_media is not None:
+                content.add_media(media=self.content_audio_media)
+
+            # sort
+            content.playlists.sort( key=lambda x: x.stream_info.bandwidth, reverse=True)
+            for index, playlist in enumerate(content.playlists):
                 print(
                     "Selected Quality: {}\n"
                     "Playlist Index: {}\n"
@@ -272,34 +308,47 @@ class download:
                     "auto" in self.entry.quality and index == 0
                 ) or self.entry.quality in playlist.stream_info.resolution:
                     try:
+                        for media in content.media:
+                            if playlist.stream_info.audio in str(media):
+                                self.content_audio_media = media
+
                         chosen_uri = content.playlists[index].uri
                         if not self._is_url(chosen_uri):
                             chosen_uri = urllib.parse.urljoin(
                                 content.base_uri, chosen_uri
                             )
+                        if self.content_audio_media is not None:
+                            media_uri = self.content_audio_media.uri
+                            self.content_audio_media = self._download_m3u8(media_uri, timeout,
+                                                                           headers, True)
                         return self._download_m3u8(chosen_uri, timeout, headers)
 
                     except (ValueError, IndexError):
                         exit("Failed to get stream for chosen quality")
 
+        else:
+            self._download_key(content)
+
         return content
 
-    def _dump_m3u8(self):
-        for index, segment in enumerate(self._m3u8_content.segments):
-            self._m3u8_content.segments[index].uri = self._get_filename(
+    def _dump_m3u8(self, content):
+        audio_suffix = ""
+        for index, segment in enumerate(content.segments):
+            content.segments[index].uri = self._get_filename(
                 segment.uri, self.temp_folder
             )
 
-        filename = self._get_filename("master.m3u8", self.temp_folder)
-        print("File Name for local m3u8 file: {}\n\n".format(filename))
-        self._m3u8_content.dump(filename)
+        if self.is_audio:
+            audio_suffix = "_audio"
+        filename = self._get_filename(f"master{audio_suffix}.m3u8", self.temp_folder)
+        content.dump(filename)
         return filename
 
-    def _download_key(self):
-        for key in self._m3u8_content.keys:
+    def _download_key(self, content):
+        for key in content.keys:
             if key:
                 uri = key.absolute_uri
-                filename = self._get_filename(uri, self.show_folder)
+                filename = self._get_filename(uri, self.temp_folder)
 
                 with self.session.get(
                     uri, timeout=10, headers=self.headers
@@ -317,7 +366,9 @@ class download:
         return re.match(r"https?://", uri) is not None
 
     @staticmethod
-    def _get_filename(uri, directory):
+    def _get_filename(uri, directory, suffix=""):
+        if suffix:
+            suffix = f"_{suffix}"
         basename = urllib.parse.urlparse(uri).path.split("/")[-1]
-        filename = Path("{}/{}".format(directory, basename)).__str__()
+        filename = Path("{}/{}{}".format(directory, basename, suffix)).__str__()
         return filename
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="anipy_cli",
     packages=find_packages(include=["anipy_cli"]),
-    version="2.4.7",
+    version="2.4.8",
     python_requires=">3.9",
     description="Little tool in python to watch anime from the terminal (the better way to watch anime)",
     long_description=long_description,
@@ -24,6 +24,8 @@ setup(
         "kitsu.py-extended",
         "pypresence",
         "python-dateutil",
+        "m3u8",
+        "tqdm",
     ],
     entry_points="[console_scripts]\nanipy-cli=anipy_cli.run_anipy_cli:main",
 )
