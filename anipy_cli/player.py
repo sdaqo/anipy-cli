615e577289398043f2beb410bdb83aa278f3acf1
Dabbing Guy <77312570+Dabbing-Guy@users.noreply.github.com>
2023-01-09 20:40:08 -0600

    Imports should be at the top of the file

--- anipy_cli/player.py
+++ anipy_cli/player.py
@@ -4,7 +4,7 @@ import sys
 import subprocess as sp
 from pypresence import Presence
 from pypresence.exceptions import DiscordNotFound
-
+import mpv
 
 from .history import history
 from .misc import get_anime_info, error
@@ -33,10 +33,10 @@ def start_player(entry, rpc_client=None, player=None):
         player_command = [
             f"{player}",
             f"{entry.stream_url}",
-             "--" if player == "syncplay" else "",
+            "--" if player == "syncplay" else "",
             f"--force-media-title={media_title}",
             f"--referrer={entry.embed_url}",
-             "--force-window=immediate",
+            "--force-window=immediate",
         ]
 
         for x in Config().mpv_commandline_options:
@@ -76,16 +76,17 @@ def start_player(entry, rpc_client=None, player=None):
 
     return sub_proc
 
+
 def create_mpv_controllable():
-    import mpv
     player = mpv.MPV(
         input_default_bindings=True,
         input_vo_keyboard=True,
         force_window="immediate",
-        osc=True
+        osc=True,
     )
 
     return player
+
 
 def mpv_start_stream(entry, player, rpc_client=None):
     media_title = (
@@ -104,6 +105,7 @@ def mpv_start_stream(entry, player, rpc_client=None):
         dc_presence(dc_media_title, entry.category_url, rpc_client)
 
     return player
+
 
 def dc_presence_connect():
     CLIENT_ID = 966365883691855942
@@ -134,4 +136,4 @@ def dc_presence(media_title, category_url, rpc_client):
 
 
 # backwards-compatability
-#mpv = start_player
+# mpv = start_player
