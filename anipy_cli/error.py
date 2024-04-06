3f45c6281196e8df46f25edf61fbb4c16ce9b9a8
sdaqo <sdaqo.dev@protonmail.com>
2024-04-07 01:42:49 +0200

    api rewrite: begin implementation of new mal client

--- anipy_cli/error.py
+++ anipy_cli/error.py
@@ -1,3 +1,6 @@
+from typing import Dict, Optional
+
+
 class BeautifulSoupLocationError(Exception):
     def __init__(self, what: str, where: str):
         super().__init__(f"Could not locate {what} at {where}")
@@ -7,6 +10,13 @@ class RequestError(Exception):
     def __init__(self, url: str, status: int):
         super().__init__(f"Could not request `{url}`, status is {status}")
 
+class MyAnimeListError(Exception):
+    def __init__(self, url: str, status: int, mal_api_error: Optional[Dict] = None) -> None:
+        error_text = f"Error requesting {url}, status is {status}."
+        if mal_api_error:
+            error_text = f"{error_text} Additional info from api {mal_api_error}"
+
+        super().__init__(error_text)
 
 class DownloadError(Exception):
     def __init__(self, message):
