a868c962d2bfca6ceee7c0e4e58f10d715ee16df
sdaqo <sdaqo.dev@protonmail.com>
2024-04-12 14:52:06 +0200

    api rewrite: move mal adapter into mal file

--- anipy_cli/mal.py
+++ anipy_cli/mal.py
@@ -1,14 +1,19 @@
 import datetime
+import Levenshtein
 from dataclasses import dataclass
 from enum import Enum
-from typing import List, Optional
+from typing import TYPE_CHECKING, List, Optional, Set
 
 from dataclasses_json import DataClassJsonMixin, Undefined
 from requests import Request, Session
 
 from anipy_cli.error import MyAnimeListError
 from anipy_cli.version import __appname__, __version__
+from anipy_cli.anime import Anime
+from anipy_cli.provider import ProviderSearchResult, FilterCapabilities, Filters, MediaType, Season
 
+if TYPE_CHECKING:
+    from anipy_cli.provider import BaseProvider
 
 class MALMyListStatusEnum(Enum):
     WATCHING = "watching"
@@ -124,6 +129,10 @@ class MyAnimeList:
 
     def get_search(self, query: str, limit: int = 20, pages: int = 1) -> List[MALAnime]:
         return self._get_resource("anime", {"q": query}, limit, pages)
+    
+    def get_anime(self, anime_id: int) -> MALAnime:
+        request = Request("GET", f"{self.API_BASE}/anime/{anime_id}")
+        return MALAnime.from_json(self._make_request(request))
 
     def get_anime_list(
         self, status_filter: Optional[MALMyListStatusEnum] = None
@@ -137,9 +146,9 @@ class MyAnimeList:
     def update_anime_list(
         self,
         anime_id: int,
-        status: Optional[MALMyListStatusEnum],
-        watched_episodes: Optional[int],
-        tags: Optional[List[str]],
+        status: Optional[MALMyListStatusEnum] = None,
+        watched_episodes: Optional[int] = None,
+        tags: Optional[List[str]] = None,
     ) -> MALMyListStatus:
         request = Request("PATCH", f"{self.API_BASE}/anime/{anime_id}/my_list_status")
         request.params.update(
@@ -243,3 +252,122 @@ class MyAnimeList:
             0, int(data["expires_in"])
         )
         self._refresh_token = data["refresh_token"]
+
+
+
+
+class MyAnimeListAdapter:
+    def __init__(self, myanimelist: MyAnimeList, provider: "BaseProvider") -> None:
+        self.mal = myanimelist
+        self.provider = provider
+    
+    @staticmethod
+    def _find_best_ratio(first_set: Set[str], second_set: Set[str]) -> float:
+        best_ratio = 0
+        for i in first_set:
+            for j in second_set:
+                r = Levenshtein.ratio(i, j, processor=str.lower)
+                if r > best_ratio:
+                    best_ratio = r
+                if best_ratio == 1:
+                    break
+            else:
+                continue
+            break
+
+        return best_ratio
+
+    def from_provider(
+        self,
+        anime: Anime,
+        minimum_similarity_ratio: float = 0.8,
+        use_alternative_names: bool = True,
+    ) -> Optional[MALAnime]:
+        results = self.mal.get_search(anime.name)
+        if use_alternative_names:
+            anime.get_info().alternative_names
+
+        best_anime = None
+        best_ratio = 0
+        for i in results:
+            titles_mal = {i.title}
+            titles_provider = {anime.name}
+
+            if use_alternative_names:
+                titles_mal |= {
+                    i.alternative_titles.ja,
+                    i.alternative_titles.en,
+                    *i.alternative_titles.synonyms,
+                }
+
+                titles_provider |= set(anime.get_info().alternative_names or [])
+
+            ratio = self._find_best_ratio(titles_mal, titles_provider)
+
+            if ratio > best_ratio:
+                best_ratio = ratio
+                best_anime = i
+
+            if best_ratio == 1:
+                break
+
+        if best_ratio >= minimum_similarity_ratio:
+            return best_anime
+
+    def from_myanimelist(
+        self,
+        mal_anime: MALAnime,
+        minimum_similarity_ratio: float = 0.8,
+        use_filters: bool = True,
+        use_alternative_names: bool = True,
+    ) -> Optional[Anime]:
+        mal_titles = {mal_anime.title}
+        if use_alternative_names:
+            mal_titles |= set(
+                [
+                    mal_anime.title,
+                    mal_anime.alternative_titles.ja,
+                    mal_anime.alternative_titles.en,
+                    *mal_anime.alternative_titles.synonyms,
+                ]
+            )
+
+        provider_filters = Filters()
+        if self.provider.FILTER_CAPS & FilterCapabilities.YEAR:
+            provider_filters.year = [mal_anime.start_season.year]
+
+        if self.provider.FILTER_CAPS & FilterCapabilities.SEASON:
+            provider_filters.season = [Season[mal_anime.start_season.season.upper()]]
+
+        if self.provider.FILTER_CAPS & FilterCapabilities.MEDIA_TYPE:
+            if mal_anime.media_type != MALMediaTypeEnum.UNKNOWN:
+                provider_filters.media_type = [
+                    MediaType[mal_anime.media_type.value.upper()]
+                ]
+
+        results: Set[ProviderSearchResult] = set()
+        for title in mal_titles:
+            results |= set(self.provider.get_search(title))
+            if use_filters:
+                results |= set(self.provider.get_search(title, provider_filters))
+        
+        best_ratio = 0
+        best_anime = None
+        for r in results:
+            anime = Anime.from_search_result(self.provider, r)
+            provider_titles = {anime.name}
+
+            if use_alternative_names:
+                provider_titles |= set(anime.get_info().alternative_names or [])
+
+            ratio = self._find_best_ratio(mal_titles, provider_titles)
+
+            if ratio > best_ratio:
+                best_ratio = ratio
+                best_anime = anime
+
+            if best_ratio == 1:
+                break
+
+        if best_ratio > minimum_similarity_ratio:
+            return best_anime
