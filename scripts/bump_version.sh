57eec45b5805d039102f2861279108681f494ffd
sdaqo <sdaqo.dev@protonmail.com>
2024-05-16 12:47:02 +0200

    tooling: add version bumping script

---
+++ scripts/bump_version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
+version=$1
+
+update_version() {
+    local file_path=$1
+    local version=$2
+
+    sed -i -e "s/__version__ = \"[^\"]*\"/__version__ = \"$version\"/" \
+           -e "s/version = \"[^\"]*\"/version = \"$version\"/" "$file_path"
+}
+
+update_version "cli/pyproject.toml" "$version"
+update_version "api/pyproject.toml" "$version"
+update_version "cli/src/anipy_cli/__init__.py" "$version"
+update_version "api/src/anipy_api/__init__.py" "$version"
+
