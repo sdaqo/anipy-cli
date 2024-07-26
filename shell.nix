c934e25fc1283277eeb02c07567ed6d652a25a5e
sdaqo <sdaqo.dev@protonmail.com>
2024-07-26 15:52:32 +0200

    misc: improve nix shell

--- shell.nix
+++ shell.nix
@@ -3,6 +3,7 @@ let
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 pkgs.mkShell {
+  LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib/:/run/opengl-driver/lib/";
   packages = with pkgs; [
     python3
     (poetry.override { python3 = python3; })
