dc8720ccd6bab11a06e09ad64432327f7d4cb987
sdaqo <sdaqo.dev@protonmail.com>
2025-04-16 09:28:14 +0200

    misc: add nix shell for key-gen

---
+++ shell.nix
@@ -0,0 +1,10 @@
+let 
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+pkgs.mkShell {
+  packages = with pkgs; [
+    nodejs
+    pnpm
+  ];
+}
