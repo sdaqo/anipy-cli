e46ec19753cabcce5560b16e3c2e0ce936bdc3e4
sdaqo <sdaqo.dev@protonmail.com>
2025-05-15 20:12:50 +0200

    misc: add animekai is not functional warning

--- cli/src/anipy_cli/config.py
+++ cli/src/anipy_cli/config.py
@@ -72,7 +72,7 @@ class Config:
 
         For an updated list of providers look here: https://sdaqo.github.io/anipy-cli/availabilty
 
-        Supported providers (as of $version): allanime, animekai
+        Supported providers (as of $version): allanime, animekai (animekai is not functional for now)
 
         Examples:
             providers:
