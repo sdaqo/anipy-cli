1d6bf6caabf3aa65758bcfceb70a0d0d1732f4e9
sdaqo <sdaqo.dev@protonmail.com>
2026-07-19 14:54:37 +0200

    misc: update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,10 +1,14 @@
 let 
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.05";
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 pkgs.mkShell {
+  LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib/:/run/opengl-driver/lib/";
   packages = with pkgs; [
-    nodejs
-    pnpm
+    python314
+    ruff
+    black
+    ffmpeg
+    (poetry.override { python3 = python3; })
   ];
 }
